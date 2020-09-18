bcc92c7558c28ff2112b371de1fa7943b7fc161b
K-ona <yt11011997@gmail.com>
2020-09-18 17:13:01 +0800

    commit to

--- Time.py
+++ Time.py
@@ -4,4 +4,9 @@ import time
 t1 = time.perf_counter()
 print(time.perf_counter()-t1)
 
-# 
+# get epoch 
+import time
+
+print(time.gmtime(0))
+print(time.gmtime()) # UTC 
+print(time.localtime()) #local time
