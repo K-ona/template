5deaaf77fb8bbfada5f51da79ca25c9268c70e7a
K-ona <38185987+K-ona@users.noreply.github.com>
2020-10-25 16:15:22 +0800

    update

---
+++ test/Test.py
@@ -0,0 +1,288 @@
+import langid
+
+content = """　　Walmart will stop selling e-cigarettes - CNN
+　　Markets
+　　Tech
+　　Media
+　　Success
+　　Perspectives
+　　Videos
+　　Edition
+　　U.S.
+　　International
+　　Arabic
+　　Español
+　　Markets
+　　Tech
+　　Media
+　　Success
+　　Perspectives
+　　Videos
+　　Search
+　　Edition
+　　U.S.
+　　International
+　　Arabic
+　　Español
+　　Markets
+　　Premarkets
+　　Dow 30
+　　After-Hours
+　　Market Movers
+　　Fear & Greed
+　　World Markets
+　　Sector Performance
+　　Investing
+　　Markets Now
+　　Before the Bell
+　　Leading Indicator
+　　Global Energy Challenge
+　　Economy
+　　Energy
+　　Tech
+　　Innovate
+　　Gadget
+　　Mission: Ahead
+　　Upstarts
+　　Business Evolved
+　　Work Transformed
+　　Innovative Cities
+　　Unhackable
+　　Media
+　　Reliable Sources
+　　Success
+　　Boss Files
+　　Fresh Money
+　　Invest Ahead
+　　Cars
+　　Homes
+　　Wealth Coach
+　　Biz + Leisure
+　　Center Piece
+　　Perspectives
+　　Videos
+　　International
+　　Switzerland
+　　India
+　　Davos
+　　Reliable Sources
+　　Passion to Portfolio
+　　On: Germany
+　　More
+　　Accessibility & CC
+　　About Us
+　　Newsletters
+　　US
+　　World
+　　Politics
+　　Business
+　　Opinion
+　　Health
+　　Entertainment
+　　Tech
+　　Style
+　　Travel
+　　Sports
+　　Videos
+　　Coupons
+　　More
+　　Follow CNNBusiness
+　　Walmart will stop selling e-cigarettes as backlash spreads
+　　ByNathaniel Meyersohn,CNN Business
+　　Updated 4:05 PM ET, Fri September 20, 2019 
+　　Chat with us in Facebook Messenger. Find out what's happening in the world as it unfolds.
+
+
+　　JUST WATCHED
+　　Michigan is first state to ban flavored e-cigarettes
+　　Replay
+　　More Videos ...
+　　MUST WATCH
+　　(16 Videos)
+
+
+　　Michigan is first state to ban flavored e-cigarettes 
+
+
+　　Watch SpaceX's Starship Mk1 partially explode during test 
+
+
+　　Americans aren't drinking milk like they used to 
+
+
+　　The moral dilemma of doing business in China, explained 
+
+
+　　America almost ran out of White Claw. Here's how companies handle shortages 
+
+
+　　This energy startup has made a solar breakthrough 
+
+
+　　Politics? VETO! UNO is now 'nonpartisan' for the holidays 
+
+
+　　FedEx CEO challenges the New York Times to debate tax policy 
+
+
+　　Marie Kondo opens online shop 
+
+
+　　T-Mobile CEO John Legere stepping down 
+
+
+　　See the first SUV with the Mustang name 
+
+
+　　Amazon to protest Pentagon's cloud contract to Microsoft 
+
+
+　　This is the world's largest Starbucks 
+
+
+　　Could mushrooms be the key to replacing plastic? 
+
+
+　　Disney is investing big in streaming. Here's why 
+
+
+　　Supreme Court allows Sandy Hook families to sue Remington 
+　　New York (CNN Business)Walmart said Friday it will stop selling e-cigarettes as the number of deaths tied to vaping grows and public officials crack down on the industry.
+　　The decision by America's largest retailer may influence other stores and marks another setback for the vaping industry. Walmart said it's exiting the business because of regulatory flux. CBS, Viacom and CNN parent company WarnerMedia also announced this week they would prohibit e-cigarette advertisements on their networks.
+　　"Given the growing federal, state and local regulatory complexity and uncertainty regarding e-cigarettes, we plan to discontinue the sale of electronic nicotine delivery products at all Walmart and Sam's Club US locations," the company said in a statement. "We will complete our exit after selling through current inventory."
+　　For Walmart, the move is another illustration of the company's willingness to wade into controversial issues. Earlier this month, Walmart announced it would stop selling handgun ammunition and ammunition commonly used in military-style assault weapons, as well as discourage customers from openly carrying guns at stores.
+　　Halting the sale of e-cigarettes is also Walmart's latest major decision in restricting the availability of tobacco.
+　　Read More
+　　Earlier this year, Walmart raised theminimum age to buy tobacco to 21. The changes took effect July 1 at all US Walmart and Sam's Club locations. The company said at the time that it would stop selling sweet-flavored e-cigarettes, which have become popular among teenagers.
+　　The vaping industry has experienced rapid growth. Euromonitor International, a research firm, estimated it will be worth$34 billionglobally by 2021, up 176% from 2016. Last year, Altria, America's preeminent cigarette company, took a 35% stake in e-cigarette maker Juul, worth $12.8 billion.
+　　Vaping industry in turmoil
+　　Walmart's decision to step away fully from e-cigarettes comes as more states restrict the products.
+　　New York state health officials voted Tuesday to carry out Gov. Andrew Cuomo's executive action to ban sales of most flavored e-cigarettes. This ban would stand for 90 days as a piece of emergency legislation. It would need to be renewed to continue. Last week, Michigan became the first state to ban the sale of flavored e-cigarettes.
+　　President Donald Trump said last week that the FDA would be putting out "some very strong recommendations" regarding the use of flavored e-cigarettes.
+　　The eighth person in the United Statesdied Thursdayfrom lung disease related to vaping, according to Missouri health officials.
+　　There have been 530 confirmed and probable cases of lung injuries related to e-cigarettes as of September 17, the Centers for Disease Control and Prevention said Thursday. That's 150 more than was reported on September 11.
+　　The US Centers for Disease Control and Prevention, the Food and Drug Administration and state health departments have been investigating this outbreak.
+　　Still, health officials said they haven't found a definitive cause or a clear connection between cases, and they warned that patients worried about becoming ill from vaping should refrain from using e-cigarettes.
+　　—CNN's Jen Christensen and Jamie Gumbrecht contributed to this article.
+　　Content by CompareCards
+　　9 cards charging 0% interest until 2021 
+　　Results are in: Top balance transfer cards of 2019 
+　　Transfer your debt and pay 0 interest until 2021 
+　　Spend $500, get $150 fast with this top card 
+　　Pay 0% Interest for 15 months with these cards 
+　　Search
+　　US
+　　Crime + Justice
+　　Energy + Environment
+　　Extreme Weather
+　　Space + Science
+　　World
+　　Africa
+　　Americas
+　　Asia
+　　Australia
+　　China
+　　Europe
+　　India
+　　Middle East
+　　United Kingdom
+　　Politics
+　　45
+　　Congress
+　　SCOTUS
+　　Facts First
+　　2020
+　　2019 Elections
+　　Business
+　　Markets
+　　Tech
+　　Media
+　　Success
+　　Perspectives
+　　Videos
+　　Opinion
+　　Political Op-Eds
+　　Social Commentary
+　　Health
+　　Food
+　　Fitness
+　　Wellness
+　　Parenting
+　　Vital Signs
+　　Entertainment
+　　Stars
+　　Screen
+　　Binge
+　　Culture
+　　Media
+　　Tech
+　　Innovate
+　　Gadget
+　　Mission: Ahead
+　　Upstarts
+　　Work Transformed
+　　Innovative Cities
+　　Style
+　　Arts
+　　Design
+　　Fashion
+　　Architecture
+　　Luxury
+　　Autos
+　　Video
+　　Travel
+　　Destinations
+　　Food & Drink
+　　Play
+　　Stay
+　　Videos
+　　Sports
+　　Pro Football
+　　College Football
+　　Basketball
+　　Baseball
+　　Soccer
+　　Olympics
+　　Videos
+　　Live TV
+　　Digital Studios
+　　CNN Films
+　　HLN
+　　TV Schedule
+　　TV Shows A-Z
+　　CNNVR
+　　Coupons
+　　CNN Underscored
+　　Explore
+　　Wellness
+　　Gadgets
+　　Lifestyle
+　　CNN Store
+　　More
+　　Photos
+　　Longform
+　　Investigations
+　　CNN Profiles
+　　CNN Leadership
+　　CNN Newsletters
+　　Work for CNN
+　　Follow CNNBusiness
+　　Most stock quote data provided by BATS. Market indices are shown in real time, except for the DJIA, which is delayed by two minutes. All times are ET. Disclaimer. Morningstar: Copyright 2018 Morningstar, Inc. All Rights Reserved. Factset: FactSet Research Systems Inc.2018. All rights reserved. Chicago Mercantile Association: Certain market data is the property of Chicago Mercantile Exchange Inc. and its licensors. All rights reserved. Dow Jones: The Dow Jones branded   indices are proprietary to and are calculated, distributed and marketed by DJI Opco, a subsidiary of S&P Dow Jones Indices LLC and have been licensed for use to S&P Opco, LLC and CNN. Standard & Poor's and S&P are registered trademarks of Standard & Poor's Financial Services LLC and Dow Jones is a registered trademark of Dow Jones Trademark Holdings LLC.  All content of the Dow Jones branded indices Copyright S&P Dow Jones Indices LLC 2018 and/or its affiliates.
+　　Terms of Use
+　　Privacy Policy
+　　Accessibility & CC
+　　AdChoices
+　　About Us
+　　CNN Studio Tours
+　　CNN Store
+　　Newsletters
+　　Transcripts
+　　License Footage
+　　CNN Newsource
+　　Sitemap
+　　©2019Cable News Network.Turner Broadcasting System, Inc. All Rights Reserved. CNN Sans ™ & © 2016 Cable News Network."""
+
+
+print(content.find('©'))
+content = content[0:content.find('©')]
+print(content)
