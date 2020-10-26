223b0f808c1b4d6998fdc0c441f8eb11546f2fb2
K-ona <38185987+K-ona@users.noreply.github.com>
2020-10-26 19:00:19 +0800

    update

--- test/Test.py
+++ test/Test.py
@@ -1,53 +1,71 @@
 from ltp import LTP
-from os import path
+from os import mkdir, path
+from stanfordcorenlp import StanfordCoreNLP as SCNLP
+
+import json
 import langid
 import xlrd as xd
 import xlwt
 import re
 import time
 
-ltp = LTP(r'D:\Data\base')
+# 参数为模型所在文件夹
+nlp_ch = LTP(r'D:\Data\base')
+nlp_en = SCNLP(r'D:\Python\stanford-corenlp-4.1.0')
+
 source_path = path.join('excel', 'test_speed.xls')
 print(source_path)
 
 file = xd.open_workbook(source_path)
-
 filedata = file.sheet_by_index(0)
 
 nrows = filedata.nrows
 ncols = filedata.ncols
 print("nrows = {}, ncols = {}".format(nrows, ncols))
 
-pattern = ['url redirect', '©', 'IMAGE SRC', '>']
-
 t1 = time.perf_counter()
-for row in range(nrows):
+
+# tmp
+filename = 'test'
+
+data_zh = []
+targetzhfile_path = 'target/zh'
+data_en = []
+targetenfile_path = 'target/en'
+
+for row in range(nrows)[1:]:
     row_content = filedata.row_values(rowx = row)
     content = row_content[5]
 
     # not en or zh
-    if langid.classify(content)[0] != 'en' and langid.classify(content)[0] != 'zh':
-        continue
-    # url redirect
-    if content.find(pattern[0]) != -1:
-        # print('url redirect: ', content)
-        continue
-    # only copyright
-    if content.find(pattern[1]) < 5 and content.find(pattern[1]) > -1:
-        # print('only copyright', content)
-        continue
-    # has img or other html labels
-    if content.find(pattern[2]) != -1:
-        content = re.sub(r'<?IMAGE SRC.*>?', '', content)
-        content = re.sub(r'<.*>', '', content)
-        # print('without img: ', content)
-        
-    # content with copyright
-    if content.find(pattern[1]) != -1:
-        content = content[0:content.find(pattern[1])]
-        # print('without copyright: ', content)
+    if langid.classify(content)[0] == 'zh':
+        seg, hidden = nlp_ch.seg([content])
+        pos = nlp_ch.pos(hidden)
+        dep = nlp_ch.dep(hidden)[0]
+        data_zh.append(dep)
+        print('nlp with Chinese')
+    else :
+        dep = nlp_en.dependency_parse(content)
+        data_en.append(dep)
+        print('nlp with English')
 
-    seg, hidden = ltp.seg([content])
-    pos = ltp.pos(hidden)
-    dep = ltp.dep(hidden)
+    print('file : row = {}'.format(row))
+    
+data_zh = json.dumps(data_zh)
+data_en = json.dumps(data_en)
+
+if not path.exists(targetzhfile_path):
+    mkdir(targetzhfile_path)
+    with open(path.join(targetzhfile_path, filename + '.txt'), 'w') as fd:
+        for str in data_zh:
+            fd.write(str)
+        fd.close()
+
+if not path.exists(targetenfile_path):
+    mkdir(targetenfile_path)
+    with open(path.join(targetenfile_path, filename + '.txt'), 'w') as fd:
+        for str in data_en:
+            fd.write(str)
+        fd.close()
+
 print((time.perf_counter()-t1) / 60)
