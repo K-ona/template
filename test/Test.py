7b99f44fef5fc8b1fd414cada517bb6bfa48f941
K-ona <38185987+K-ona@users.noreply.github.com>
2020-12-24 18:17:30 +0800

    update

--- test/Test.py
+++ test/Test.py
@@ -1,20 +1,8 @@
-from openie import StanfordOpenIE
+import numpy as np
+import torch
+import math
 
-with StanfordOpenIE('stanford-corenlp-latest') as client:
-    text = 'Barack Obama was born in Hawaii. Richard Manning wrote this sentence.'
-    print('Text: %s.' % text)
-    for triple in client.annotate(text):
-        print('|-', triple)
-
-    graph_image = 'graph.png'
-    client.generate_graphviz_graph(text, graph_image)
-    print('Graph generated: %s.' % graph_image)
-
-    with open('corpus/pg6130.txt', 'r', encoding='utf8') as r:
-        corpus = r.read().replace('\n', ' ').replace('\r', '')
-
-    triples_corpus = client.annotate(corpus[0:50000])
-    print('Corpus: %s [...].' % corpus[0:80])
-    print('Found %s triples in the corpus.' % len(triples_corpus))
-    for triple in triples_corpus[:3]:
-        print('|-', triple)
+data = torch.from_numpy(np.random.random(size=(10, 10))) * 10
+print(data.size())
+print(data.unsqueeze(-2).size())
+pass
