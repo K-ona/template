261d3a16be5bd0fde275ecc4e4d084f313616642
K-ona <38185987+K-ona@users.noreply.github.com>
2020-12-29 19:20:28 +0800

    update

--- test/Test.py
+++ test/Test.py
@@ -2,9 +2,9 @@ import numpy as np
 import torch
 import math
 from collections import defaultdict
+import json
 
 d = defaultdict(int)
 d[('sds', 'sas')] = 2
 d[('sds', 'ss')] = 3
-print(d)
-print(set([x[0] for x in list(d.items())]))
+print()
