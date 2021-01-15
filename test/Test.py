827f599d03b6c3573773bb59d99206db4b8899ba
Kona1956 <18481860700>
2021-01-15 10:41:16 +0800

    logging

--- test/Test.py
+++ test/Test.py
@@ -1,10 +1,2 @@
-import numpy as np
-import torch
-import math
-from collections import defaultdict
-import json
-
-d = defaultdict(int)
-d[('sds', 'sas')] = 2
-d[('sds', 'ss')] = 3
-print()
+s = 'sdsad'
+print([s])
